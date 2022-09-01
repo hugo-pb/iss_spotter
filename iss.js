7b3b7ade017e63413ba5be81dadc316631547700
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-01 16:47:48 -0600

    1st try

--- iss.js
+++ iss.js
@@ -2,6 +2,14 @@ const request = require("request");
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
+  request(`https://api.ipify.org?format=jsonp&callback=getip`, function(
+    error,
+    response,
+    body
+  ) {
+    data = body.ip;
+    callback(error, data);
+  });
 };
 
 module.exports = { fetchMyIP };
